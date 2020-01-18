26e9f02891a5c4fef3630705bb828b1874d06834
Vuejs <vuejs@zMacPro-5.local>
2020-01-18 11:13:02 +0100

    init wizard card

---
+++ wizard-card/wizard-card/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <WizardCard/>
+  </div>
+</template>
+
+<script>
+import WizardCard from './components/WizardCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    WizardCard
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ wizard-card/wizard-card/src/components/WizardCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ 'Your mage ID' }}</h1>
+<form class="flex card"> 
+   <input v-model="wizard.name" placeholder="Add your name">
+   <input v-model="wizard.alias" placeholder="What is your alias?">
+   <input v-model="wizard.role" placeholder="What do you do?">
+   <input v-model="wizard.house" placeholder="Tell me your house">
+   <input v-model="wizard.wand" placeholder="Show off your wand">
+   </form>
+   <section class="flex">
+    <p>Name: {{ wizard.name }}</p>
+    <p>Alias: {{ wizard.alias }}</p>
+    <p>Role: {{ wizard.role }}</p>
+    <p>House: {{ wizard.house }}</p>
+    <p>Wand: {{ wizard.wand }}</p>
+</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WizardCard',
+  data(){
+    return {
+      wizard: {
+        name: '',
+        alias: '',
+        role: '',
+        house: '',
+        wand: '',
+      }
+
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.card{
+      width: 300px;
+}
+.flex{
+  display: flex;
+  flex-direction: column;
+}
+</style>
