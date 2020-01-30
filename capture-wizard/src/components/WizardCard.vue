ae2d5abbd37f9ab49c995203203e7771817f4e8f
emma-martin <emmartin@paradigmadigital.com>
2020-01-30 08:38:09 +0100

    return filteredlist

--- capture-wizard/src/components/WizardCard.vue
+++ capture-wizard/src/components/WizardCard.vue
@@ -1,42 +1,36 @@
 <template>
+  <div>
+    <!-- <img src="../assets/poster.png" alt="wizard" /> -->
+    <img :src="card.pic" alt="wizard" />
+    <p>{{card.name}}</p>
+    <p>{{card.price}}</p>
     <div>
-        <img src="../assets/poster.png" alt="wizard">
-        <img :src="card.pic" alt="wizard">
-        <p>{{card.name}}</p>
-        <p>{{card.price}}</p>
-        <div>
-            <select v-model="card.isFound" name="card" id="card">
-                <option value=false>
-                    Missing
-                </option>
-                <option value=true>
-                    Found
-                </option>
-            </select>
-            <button @click="this.$emit('delete-wizard', card.id)">
-                Delete Wizard
-            </button>
-        </div>
+      <select v-model="card.isFound" name="card" id="card">
+        <option value="false">Missing</option>
+        <option value="true">Found</option>
+      </select>
+      <button @click="deleteItem(card)">Delete Wizard</button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'WizardCard',
-        props: {
-            card: {
-            type: Object,
-            default: ()=>({})
-             },
-        },
-        data() {
-            return {
-            }
-        },
+export default {
+  name: "WizardCard",
+  props: {
+    card: {
+      type: Object,
+      default: () => ({})
     }
+  },
+  methods: {
+    deleteItem() {
+      this.$emit("delete-wizard", this.card);
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-
 </style>
 
 composition API en vue3 reemplazaran a los mixinsm
